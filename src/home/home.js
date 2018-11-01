fc9d810d8d809a5906c0fc719e6a95b7b75f07e8
Ashishs1390 <ashishhsharma1990@gmail.com>
2018-11-01 10:03:29 +0530

    old ui

--- src/home/home.js
+++ src/home/home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Router, Route, browserHistory, Link } from 'react-router-dom';
 
-import TodoComponent from '../todocomponents/todocomponent';
 
 class Home extends React.Component {
     render(){
         return(
             <div>
                 <h1>Home</h1>
-                <TodoComponent/>
             </div>
 
         )
